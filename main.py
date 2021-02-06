20dd1aeba6526dabd6c16ff0e9182b5332a119ca
nmparfait <mbamu.developer@gmail.com>
2021-02-06 08:59:30 +0000

    to do list built and impport logo and data

---
+++ main.py
@@ -0,0 +1,29 @@
+#Display art.
+
+# Generate a random account from game.
+
+#Format the account data into printable.
+
+#Ask yser for a guess.
+
+#Check if user is correct.
+
+
+#Get follower count of each account.
+
+#Use if statement to check if user is correct
+
+
+#Give user feedback on their guess.
+
+
+#Score keeping
+
+
+#Make the game repeatable
+
+
+#Making the account at position B become the next account at position A
+
+#Clear the screen 
+
