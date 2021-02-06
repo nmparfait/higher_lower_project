89b12c1c362af472d878763e5b3195c41d32b711
nmparfait <mbamu.developer@gmail.com>
2021-02-06 09:37:07 +0000

    New function to check the answer

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def format_data(account):
   account_descr= account["description"]
   account_country = account["country"]
   return f"{account_name}, a {account_descr}, from {account_country}"
-    
+
+def check_answer(guess, a_followers, b_followers):
+  """Take the user guess and follower counts and returns if they got it right. """
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
 #Display art.
 print(logo)
 # Generate a random account from game.
@@ -28,13 +36,14 @@ print(f"Compare become: {format_data(account_b)}.")
 
 
 
-#Ask yser for a guess.
+#Ask user for a guess.
+guess = input("Who has more followers? Type 'A' or 'B'.").lower()
+
 
 #Check if user is correct.
-
-
 #Get follower count of each account.
-
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"] 
 #Use if statement to check if user is correct
 
 
