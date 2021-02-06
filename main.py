5436574918ddda753d060d604a10ab60a16b036c
nmparfait <mbamu.developer@gmail.com>
2021-02-06 09:18:53 +0000

    comparing the first description to the second

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+
+from art import logo, vs
+from game_data import data
+import random 
+
+def format_data(account):
+  """Format the account data into printable format"""
+  account_name = account["name"]
+  account_descr= account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_descr}, from {account_country}"
+    
 #Display art.
-
+print(logo)
 # Generate a random account from game.
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+  account_b = random.choice(data)
 
-#Format the account data into printable.
+print(f"Compare A: {format_data(account_a)}.")
+print(vs)
+print(f"Compare become: {format_data(account_b)}.")
+
+#Format the account data into printable format.
+
+
+
+
 
 #Ask yser for a guess.
 
